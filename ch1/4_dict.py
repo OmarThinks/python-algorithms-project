5ec1e51fb69e67b4e5c53b6996ecc57f77afcc72
Omar Magdy <contact.parmag@gmail.com>
2021-10-09 22:01:27 +0200

    dict keys and values

---
+++ ch1/4_dict.py
@@ -0,0 +1,14 @@
+dict_1 = {"key1":123,"key2":456}
+
+print(list(dict_1.keys())) 	# ['key1', 'key2']
+print(list(dict_1.values()))# [123, 456]
+
+
+for key in dict_1.keys():
+	print(key)
+
+
+"""
+key1
+key2
+"""
