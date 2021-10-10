a67c9c962a971003ed291793ac3f2e619e9d5c6a
Omar Magdy <contact.parmag@gmail.com>
2021-10-10 10:43:42 +0200

    min_in_list_linear

--- ch2/1_min_in_list.py
+++ ch2/1_min_in_list.py
@@ -20,6 +20,12 @@ This will have a Quadratic Complexity
 from math import inf
 
 
+"""
+This will have a Quadratic Complexity
+"O(n^2)"
+"""
+
+
 def find_min_in_list_quadratic(input_list):
 	min_number = inf
 	
@@ -36,6 +42,30 @@ assert(find_min_in_list_quadratic([1]) == 1)
 assert(find_min_in_list_quadratic([1,2,3]) == 1)
 assert(find_min_in_list_quadratic([2,3,4,5]) == 2)
 
+
+
+
+
+
+
+"""
+This will have a Linear Complexity
+"O(n)"
+"""
+
+def find_min_in_list_linear(input_list):
+	min_number = inf
+	for element in input_list:
+		if element<min_number:
+			min_number=element
+	return min_number
+
+
+
+assert(find_min_in_list_linear([]) == inf)
+assert(find_min_in_list_linear([1]) == 1)
+assert(find_min_in_list_linear([1,2,3]) == 1)
+assert(find_min_in_list_linear([2,3,4,5]) == 2)
 
 
 
